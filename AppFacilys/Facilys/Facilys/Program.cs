4da0358c761c839dce1b0d6348afa9a16bdbede0
Filleux <filleux.dim60@hotmail.com>
2024-11-12 12:19:43 +0100

    Init Blazor web app

---
+++ AppFacilys/Facilys/Facilys/Program.cs
@@ -0,0 +1,50 @@
+using Facilys.Components;
+using ElectronNET.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuer Electron
+builder.WebHost.UseElectron(args);
+builder.Services.AddElectron();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<Facilys.Components.App>()
+    .AddInteractiveServerRenderMode();
+
+// Créer la fenêtre Electron
+async Task CreateElectronWindow()
+{
+    var window = await Electron.WindowManager.CreateWindowAsync();
+    window.OnClosed += () => Electron.App.Quit();
+}
+
+// Vérifiez si l'application s'exécute dans Electron
+if (HybridSupport.IsElectronActive)
+{
+    Task.Run(async () => {
+        var window = await Electron.WindowManager.CreateWindowAsync();
+        window.OnClosed += () => {
+            Electron.App.Quit();
+        };
+    });
+}
+
+app.Run();
